8b5d87116722165d123b843c8296445677756e21
abisummers <hello@abisummers.com>
2018-09-27 18:27:28 +0200

    login route added

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcyrpt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 const User = require("../public/models/user-model");
@@ -10,12 +10,37 @@ router.post("/signup", (req, res, next) => {
   // console.log(req.body);
   const { fullName, email, originalPassword } = req.body;
 
-  const encryptedPassword = bcyrpt.hashSync(originalPassword, 10);
+  const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
 
   User.create({ fullName, encryptedPassword, email })
     .then(userDoc => {
       userDoc.encryptedPassword = undefined;
       res.json({ userDoc });
+    })
+    .catch(err => next(err));
+});
+
+//-------------------------- LOGIN -------------------------------
+
+router.post("/login", (req, res, next) => {
+  const { email, originalPassword } = req.body;
+
+  User.findOne({ email: { $eq: email } })
+
+    .then(userDoc => {
+      if (!userDoc) {
+        next(new Error("incorrect email"));
+        return;
+      }
+      const { encryptedPassword } = userDoc;
+      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+        next(new Error("Wrong password"));
+        return;
+      }
+      req.logIn(userDoc, () => {
+        userDoc.encryptedPassword = undefined;
+        res.json({ userDoc });
+      });
     })
     .catch(err => next(err));
 });
