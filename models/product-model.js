bae301a097c139345fee82e056983a66c19b615b
abisummers <hello@abisummers.com>
2018-10-01 14:44:12 +0200

    updated product model

--- models/product-model.js
+++ models/product-model.js
@@ -1,25 +1,58 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-    datasetid: {type: String},
-    recordid:{type:String},
-    fields:{
-   
-            qu_est_ce_que_j_en_fais: { type: String, enum: ["associations", "bois de chauffage", "composteur", "déchèterie", "garagiste", "magasins de la grande distribution", "opticien", "ordures ménagères", "pharmacie", "tri(conteneur de verre)", "tri (sac - bac - point d'apport volontaire)"], required: true },
-            comment_eviter_de_le_produire: { type: String },
-            produits: { type: String, required: true },
-            que_va_t_il_devenir: { type: String, required: true },
-            images: { type: String, required: true },
-            typologie_des_dechets: { type: String, enum: ["Bois", "Déchets du Bâtiment", "Déchets de cuisine", "Déchets Dangereux", "Déchets Electriques et électroniques", "Divers", "Métaux", "Mobilier", "Papiers-Cartons", "Plastiques", "Textiles et cuir", "Verre"], required: true },
-            },   
-    record_timestamp:{type:String} 
-},
-{
-    collection:"recycling"
-}
+const productSchema = new Schema(
+  {
+    datasetid: { type: String },
+    recordid: { type: String },
+    fields: {
+      qu_est_ce_que_j_en_fais: {
+        type: String,
+        enum: [
+          "associations",
+          "bois de chauffage",
+          "composteur",
+          "déchèterie",
+          "garagiste",
+          "magasins de la grande distribution",
+          "opticien",
+          "ordures ménagères",
+          "pharmacie",
+          "tri(conteneur de verre)",
+          "tri (sac - bac - point d'apport volontaire)"
+        ],
+        required: true
+      },
+      comment_eviter_de_le_produire: { type: String },
+      produits: { type: String, required: true },
+      que_va_t_il_devenir: { type: String, required: true },
+      images: { type: String, required: true },
+      typologie_des_dechets: {
+        type: String,
+        enum: [
+          "Bois",
+          "Déchets du Bâtiment",
+          "Déchets de cuisine",
+          "Déchets Dangereux",
+          "Déchets Electriques et électroniques",
+          "Divers",
+          "Métaux",
+          "Mobilier",
+          "Papiers-Cartons",
+          "Plastiques",
+          "Textiles et cuir",
+          "Verre"
+        ],
+        required: true
+      }
+    },
+    record_timestamp: { type: String },
+    urlId: { type: String, required: true }
+  },
+  {
+    collection: "recycling"
+  }
 );
-
 
 const recycling = mongoose.model("recycling", productSchema);
 
--- routes/searchResult-router.js
+++ routes/searchResult-router.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const recycling = require("../public/models/product-model");
+const recycling = require("../models/product-model");
 
-router.get("/search-result", (req, res, next)=> {
-    recycling.find()
-        .then(productsArray => {
-            // console.log("blah blah blah",productsArray)
-            res.json(productsArray)
-        })
-        .catch(err => next(err));
-  });
+router.get("/search-result", (req, res, next) => {
+  recycling
+    .find()
+    .then(productsArray => {
+      // console.log("blah blah blah",productsArray)
+      res.json(productsArray);
+    })
+    .catch(err => next(err));
+});
 
-  module.exports = router;
+module.exports = router;
