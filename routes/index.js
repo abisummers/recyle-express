138bf905726b65c270577fcc313b100276971430
abisummers <hello@abisummers.com>
2018-10-02 12:53:03 +0200

    added route for fun facts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
-
+const FunFacts = require("../models/funFact-model");
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json({ hello: "hello" });
 });
 
+// --------------- FUN FACTS -----------------
+
+router.get("/facts", (req, res, next) => {
+  FunFacts.find()
+    .then(facts => {
+      res.json(facts);
+    })
+
+    .catch(err => next(err));
+});
 module.exports = router;
